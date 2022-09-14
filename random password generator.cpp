c826a695303c1b76f8239a1edcc7a88f31b37af3
Forehm <112934317+Forehm@users.noreply.github.com>
2022-09-14 17:12:24 +0300

    Add files via upload

---
+++ random password generator.cpp
@@ -0,0 +1,28 @@
+﻿
+
+
+
+
+
+
+
+
+
+
+std::string RandPass(int number_of_symbols)
+{
+	srand(time(NULL));
+	char arr[] = { 'q', 'w', 'e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m','1','2','3','4','5','6','7','8','9','0' };
+	std::string pass;
+
+	for (int i = 0; i < number_of_symbols; i++)
+	{
+		pass += arr[rand() % 36];
+	}
+
+
+	return pass;
+}
+
+
+
